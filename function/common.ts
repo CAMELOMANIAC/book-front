c85cb606126be0392abc3cf6e0f2e4549a78dfbe
yugyusang <yugyusang12@gmail.com>
2025-03-29 17:45:20 +0900

    refactor: 백엔드 연결로 인해 더이상 사용되지 않는 타입 및 함수 제거

--- function/common.ts
+++ function/common.ts
@@ -1,5 +1,3 @@
-import { KaKaoBookResponse } from "@/types/api";
-
 export const formatDateToKorean = (date: Date) => {
   const year = date.getFullYear();
   const month = date.getMonth() + 1; // 월은 0부터 시작하므로 1을 더해줍니다.
@@ -12,20 +10,18 @@ export const isMobile = () => {
   return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
 };
 
-export const handleFetchKaKaoData = async (
-  query: string | undefined,
-  target: "title" | "isbn" | "publisher" | "person"
-) => {
-  if (!query) return;
-  const response = await fetch(`${process.env.NEXT_PUBLIC_KAKAO_SEARCH_API_URL}/book?target=${target}&query=${query}`, {
-    method: "GET",
-    headers: {
-      Authorization: `KakaoAK ${process.env.NEXT_PUBLIC_KAKAO_SEARCH_API_KEY}`,
-    },
-    cache: "force-cache", //정적인 정보라 카카오가 수정하지 않을것임을 감안하고 캐싱
-    next: { revalidate: 86400, tags: [query, target] },
-  });
-  if (!response.ok) return;
-  const data: KaKaoBookResponse = await response.json();
-  return data;
+export const fetchbookDetailData = async (id?: string) => {
+  if (!id) throw new Error("id가 필요합니다");
+  try {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/books/detail/` + id, {
+      cache: "force-cache",
+      next: { revalidate: 86400, tags: ["bookDetail", id] },
+    });
+    if (!response.ok) throw new Error("통신 실퍠:" + response.status);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("에러발생", error);
+    throw error;
+  }
 };
