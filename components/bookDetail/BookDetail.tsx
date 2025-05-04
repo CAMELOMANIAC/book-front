284a15283da4a4f76acbd23f5856f0fb948126ed
yugyusang <yugyusang12@gmail.com>
2025-05-04 16:46:31 +0900

    feat: BookDetail에 fetchBookDetail적용

--- components/bookDetail/BookDetail.tsx
+++ components/bookDetail/BookDetail.tsx
@@ -1,43 +1,43 @@
-import React, { HTMLAttributes, Suspense } from "react";
+import React, { HTMLAttributes } from "react";
 import BookDetailImageSection from "./BookDetailImageSection";
 import BookTitleSection from "./BookTitleSection";
 import BookDetailSection from "./BookDetailSection";
 import RecommandedItem from "./RecommandedItem";
-import { BookDto } from "@/types/dto";
+import fetchBookDetail from "@/function/fetch/fetchBookDetail";
 
 type Props = {
   className?: string;
-  bookData: BookDto;
+  isbn: number;
 } & HTMLAttributes<HTMLDivElement>;
-const BookDetail = ({ className, bookData, ...props }: Readonly<Props>) => {
-  const kstBirthDay = new Date(new Date(bookData?.publishedDate).getTime() + 9 * 60 * 60 * 1000);
+const BookDetail = async ({ className, isbn, ...props }: Readonly<Props>) => {
+  const bookData = await fetchBookDetail(isbn);
+  if (bookData === undefined) return;
+  const kstBirthDay = new Date(new Date(bookData.publishedDate).getTime() + 9 * 60 * 60 * 1000);
 
   return (
     <div className={`relative flex size-full flex-col px-[var(--client-layout-margin)] ${className || ""}`} {...props}>
-      <BookDetailImageSection imageUrl={bookData?.titleImage} birthDay={kstBirthDay} className="my-6" />
+      <BookDetailImageSection imageUrl={bookData.titleImage} birthDay={kstBirthDay} className="my-6" />
       <BookTitleSection
         bookName={bookData?.title || "책 이름(정보 미제공)"}
         birthDayDate={kstBirthDay}
-        url={`https://search.daum.net/search?w=bookpage&bookId=${bookData?.isbn}&tab=introduction`}
+        url={`https://search.daum.net/search?w=bookpage&bookId=` + isbn + `&tab=introduction`}
         className="mb-10"
       />
       <BookDetailSection
-        author={bookData?.authorList.join(", ") || "저자 이름(정보 미제공)"}
+        author={bookData?.authorList.map((item) => item.name).join(", ") || "저자 이름(정보 미제공)"}
         pulisher={bookData?.publisher.name || "출판사(정보 미제공)"}
         description={bookData?.summary || "책 소개(정보 미제공)"}
-        viewMoreUrl={`https://search.daum.net/search?w=bookpage&bookId=${bookData?.isbn}&tab=introduction`}
+        viewMoreUrl={`https://search.daum.net/search?w=bookpage&bookId=` + isbn + `&tab=introduction`}
         className="mb-10"
       />
       <section>
         <h2 className="text-lg font-bold">추천 콘텐츠</h2>
-        <Suspense>
-          <RecommandedItem
-            imageUrl="https://s3-alpha-sig.figma.com/img/8d4f/4b13/868fb4811d7171eef83e5a8d8988ba13?Expires=1740960000&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=ORxF1blpY9piMcDNWkmkCr~abQC~WhkmoV7bR8-1ZScEsgdDOEC~8K-fqVZWqkV9CCAm9yq5kkVK41L-FC8CaWHMeg6-jXLoMccBB0h3L1UDZNW94CxQGXq5eaniifhL65J3yI-yzkLhNthlH7kj2vrEZSg3GNDkOdnznkIaG6VqGoOfiPwqKIsJpo0oDNGmYpaFecaKCtQwvXD43jaZXmvwL1jIXwFuNrG7wTJkvnEYNf8CkWfCTxBExvqEiqwaj94XxeOpGA1wWWAmyPVGqG1Ngm2sifZWw3XkNGnZs0FDqd25Wo0eZHpTRAG7nFIzAq81iB2WBEz~prRFDvoNnw__"
-            sourceName="유튜브"
-            sourceUrl="ㅁㄴㅇㄹ"
-            title="fasdfasdfasdfasdsdfasdfasdfasdfasdfasdfasdfaasdsdfasdfasdfasdfasdfasdfasdfassdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfafasd"
-          />
-        </Suspense>
+        <RecommandedItem
+          imageUrl="https://s3-alpha-sig.figma.com/img/8d4f/4b13/868fb4811d7171eef83e5a8d8988ba13?Expires=1740960000&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=ORxF1blpY9piMcDNWkmkCr~abQC~WhkmoV7bR8-1ZScEsgdDOEC~8K-fqVZWqkV9CCAm9yq5kkVK41L-FC8CaWHMeg6-jXLoMccBB0h3L1UDZNW94CxQGXq5eaniifhL65J3yI-yzkLhNthlH7kj2vrEZSg3GNDkOdnznkIaG6VqGoOfiPwqKIsJpo0oDNGmYpaFecaKCtQwvXD43jaZXmvwL1jIXwFuNrG7wTJkvnEYNf8CkWfCTxBExvqEiqwaj94XxeOpGA1wWWAmyPVGqG1Ngm2sifZWw3XkNGnZs0FDqd25Wo0eZHpTRAG7nFIzAq81iB2WBEz~prRFDvoNnw__"
+          sourceName="유튜브"
+          sourceUrl="ㅁㄴㅇㄹ"
+          title="fasdfasdfasdfasdsdfasdfasdfasdfasdfasdfasdfaasdsdfasdfasdfasdfasdfasdfasdfassdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfafasd"
+        />
       </section>
     </div>
   );
--- function/fetch/fetchBookDetail.ts
+++ function/fetch/fetchBookDetail.ts
@@ -1,2 +1,18 @@
-const fetchBookDetail = async (isbn: string) => {};
+import { BookDto } from "@/types/dto";
+
+const fetchBookDetail = async (isbn: number) => {
+  try {
+    const response = await fetch(process.env.NEXT_PUBLIC_BACKEND_URL + "/book/" + isbn, {
+      next: { revalidate: 86400, tags: ["book", isbn.toString()] },
+    });
+    if (response.ok) {
+      const result: BookDto = await response.json();
+      return result;
+    } else {
+      throw new Error(response.statusText);
+    }
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+};
 export default fetchBookDetail;
